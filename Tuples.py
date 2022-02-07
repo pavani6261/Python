db6144be7f3d981ce9a695f638d29d610c27e939
pavani6261 <98078064+pavani6261@users.noreply.github.com>
2022-02-07 17:06:09 +0530

    program to create and display tuples

---
+++ Tuples.py
@@ -0,0 +1,12 @@
+tuple= ("sun","mon","Tue","Wed","Thu","Fri","Sat")
+tup2=(23,'term',34)
+
+print(tuple)
+print(tup2)
+print(tuple + tup2)
+
+for i in tuple:
+    print(i)
+print()
+
+# tuple.pop() #  tuples are imutable
