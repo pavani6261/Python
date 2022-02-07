b21465d048c3ccf9250a7459b57ee8f8e21367d4
pavani6261 <98078064+pavani6261@users.noreply.github.com>
2022-02-07 17:07:29 +0530

    largest number using arrays

---
+++ LargestOfNum.py
@@ -0,0 +1,24 @@
+# a=[2,4,6,8,1,9,5]
+# l=0
+# for i in range(len(a)):
+#     if a[i]>l:
+#         l=a[i]
+# print(l,"is largest number")
+
+# a=[]
+# for i in range(0,6):
+#     l=int(input())
+#     a.append(l)
+# print(a)
+# l=0
+# for i in a:
+#     if i>l:
+#         l=i
+# print(l,"is largest number")
+
+
+l=0
+a=int(input())
+if a>l:
+    l=a
+print(l, 'is greater')
