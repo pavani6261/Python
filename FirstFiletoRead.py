653b00dc9a64a7373b1ba3fdcfb8ed5f5f6f29a8
pavani6261 <98078064+pavani6261@users.noreply.github.com>
2022-02-10 10:22:55 +0530

    to open , write, read and count the number of lines in the file

---
+++ FirstFiletoRead.py
@@ -0,0 +1,16 @@
+# open and append text to the file
+def Fileopen(filename):
+    # with open(filename, "a") as f1:    #open the file with name f1 and close the file after executing given lines in it
+    #     f1.write("\nHi all\n")     #write() used to write the content into the file
+    temp = open(filename)
+    print(temp.read())  #read()- this function used to read the content in the file
+    temp.close()
+Fileopen('FirstFile.txt')
+
+def FileCount(filename):
+    temp = open(filename)  #open the file
+    for i, l in enumerate(temp):  
+        pass
+    return i+1
+    temp.close()  #to close the file
+print("number of lines in the file are:",FileCount('FirstFile.txt'))
