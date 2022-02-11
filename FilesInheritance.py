1aeb4e70f39df217687ba8df4318bff9c9e7e04b
pavani6261 <98078064+pavani6261@users.noreply.github.com>
2022-02-11 20:04:06 +0530

    Add files via upload

--- FilesInheritance.py
+++ FilesInheritance.py
@@ -10,22 +10,23 @@ class DerivedCls1(Base):
         self.filename = filename
         Base.FileOpen(self, self.filename)
     def FileWrite(self,new2):
-        self.new_data = {
-            "name": "Davin",
-            "id": "20",
-            "age": "34"
-        }
+        # self.new_data = {
+        #     "name": "Davin",
+        #     "id": "20",
+        #     "age": "34"
+        # }
         self.new2 = new2
         with open(self.filename,"r+") as file:
             self.data["employee"].append(self.new2)
             file.seek(0)
             json.dump(self.data,file,indent=4)
 
-c = DerivedCls1('JsonDataFile1.json')
+c = DerivedCls1('JsonDataFile1.json')  #reading the content
 a= {
-    "name" : "Hema",
-    "id" : "19",
-    "age" : "35"
+    "name" : "Rema",
+    "id" : "12",
+    "age" : "32"
 }
-c.FileWrite(a)
-c.FileOpen('JsonDataFile1.json')
+c.FileWrite(a)  #appending the data
+print("after appending")
+c.FileOpen('JsonDataFile1.json') #to read the file again
